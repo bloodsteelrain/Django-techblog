b9b6950fa8ff2b8246cbec5ba70de73a7950c0d6
bloodsteelrain <chwoo9528@gmail.com>
2023-07-17 16:24:58 +0900

    게시글 읽기 및 메인 페이지 구현

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 app_name = 'blog'
 
-urlpatterns = []
+urlpatterns = [
+    # 글 목록 조회
+    path('', views.Index.as_view(), name='list'),
+    path('detail/<int:pk>/', views.DetailView.as_view(), name='detail'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Post
 
 # Create your views here.
+
+
+# ## 게시글
+class Index(View):
+    def get(self, request):
+        posts = Post.objects.all()
+        context = {
+            'title': 'Blog',
+            'posts': posts,
+        }
+        return render(request, 'blog/post_list.html', context)
+
+
+class DetailView(View):
+    def get(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        context = {
+            'title': 'Blog',
+            'post': post,
+        }
+        return render(request, 'blog/post_detail.html', context)
