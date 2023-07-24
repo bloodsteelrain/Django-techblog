a152f1df183206aad73fffa26c80616d03b17466
bloodsteelrain <chwoo9528@gmail.com>
2023-07-24 21:51:24 +0900

    게시글 목록 페이지 중 게시글 목록 부분 css 연결

--- app/settings.py
+++ app/settings.py
@@ -124,6 +124,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Media URL
 MEDIA_URL = '/media/'
